aca0031ef0dd4d08f76a4071eccfc99260b48037
DenVoronin <voronin.d93@mail.ru>
2022-06-28 19:37:23 +0300

    1

--- src/main/java/moySkladTest/Main.java
+++ src/main/java/moySkladTest/Main.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("moySkladTest")
 public class Main {
-
+//test
     public static void main(String[] args){
         SpringApplication.run(Main.class, args);
     }
